0bf989fbcf4990976a9d450252ae089681fa7624
ze <ze.lupicaetano@gmail.com>
2026-09-09 13:04:18 +0100

    bodySizeLimit fix

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["better-sqlite3"],
-  experimental: { cpus: 1, webpackBuildWorker: false, serverActions: { bodySizeLimit: "101mb" } },
+  experimental: { cpus: 1, webpackBuildWorker: false },
+  serverActions: { bodySizeLimit: "101mb" },
   async headers() {
     return [
       {
         source: "/sw.js",
         headers: [
-          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
           { key: "Service-Worker-Allowed", value: "/" },
         ],
       },
-      { source: "/offline.html", headers: [{ key: "Cache-Control", value: "no-store" }] },
-    ];
+      {
+        source: "/offline.html",
+        headers: [{ key: "Cache-Control", value: "no-store" }],
+      },
+    ]
   },
-};
+}
 
 export default nextConfig;
